cd7f1e2f350afb9cb3b501ef2072b5489b9654c1
Ramón Huidobro <hola@ramonh.dev>
2023-05-23 17:13:42 +0200

    Working on stuff

--- src/components/RandomTalk.jsx
+++ src/components/RandomTalk.jsx
@@ -4,12 +4,22 @@ import { useState } from 'preact/hooks';
 export default function RandomTalk({talks}) {
 
   const randomTalk = () => talks[(Math.floor(Math.random() * talks.length))];
-  
+
   const [talk, setTalk] = useState(talks[0]);
 
+  const { slug, data: { title, event, year } } = talk
+
   return (
-    <div> 
-      <h1>{video}!</h1>
+    <div>
+      <h2>{title}!</h2>
+      <a href={`/talks/${slug}`}>
+        <img src={`https://img.youtube.com/vi/<insert-youtube-video-id-here>/mqdefault.jpg`}/>
+      </a>
+          <Card
+            href={}
+            title={title}
+            body={`${event}, ${year}`}
+          />
       <button onClick={() => setTalk(randomTalk())}>
         Gimme Another Talk!
       </button>
